b835793c185ca788e3546b20e2d2b36a4b553d4b
Kevin Hang <kevinhang.kh@gmail.com>
2024-01-16 17:41:27 +0100

    Implement Error and Loading screen and unit tests. Add styling.

--- src/components/Rooms/Rooms.tsx
+++ src/components/Rooms/Rooms.tsx
@@ -1,14 +1,69 @@
-import { Box, Typography } from '@mui/material';
+import { Box, CircularProgress, Typography } from '@mui/material';
 import { ReactElement } from 'react';
+
 import useRooms, { IUseRooms } from '../../hooks/useRooms';
 
 export default function Rooms(): ReactElement {
   const { rooms, loading, error }: IUseRooms = useRooms();
 
+  const Header = (): ReactElement => (
+    <Box
+      width="100%"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="start"
+      // boxSizing="border-box"
+      data-testid="rooms-header"
+    >
+      <Typography variant="h3">Rooms</Typography>
+      <Box>
+        <Typography variant="h5" color="#9A9A9A">
+          Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor
+          <br />
+          ultricies donec risus sodales. Tempus quis et.
+        </Typography>
+      </Box>
+    </Box>
+  );
+
+  const Error = (): ReactElement => (
+    <Box data-testid="rooms-error">
+      <Typography variant="h6" color="#D94C4B">
+        Oops, an error occurred... please try again.
+      </Typography>
+    </Box>
+  );
+
+  const Loading = (): ReactElement => (
+    <Box data-testid="rooms-loading">
+      <CircularProgress sx={{ color: 'black' }} />
+    </Box>
+  );
+
   return (
-    <Box>
-      <Typography variant="h4">Rooms</Typography>
-      <Box>{JSON.stringify(rooms)}</Box>
+    <Box
+      width="100%"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={6}
+      padding={4}
+      boxSizing="border-box"
+      data-testid="rooms-box"
+    >
+      <Header />
+
+      <Box>
+        {loading ? (
+          <Loading />
+        ) : error ? (
+          <Error />
+        ) : (
+          <Box data-testid="rooms-list">{JSON.stringify(rooms)}</Box>
+        )}
+      </Box>
     </Box>
   );
 }
---
+++ src/components/Rooms/__tests__/Rooms.test.tsx
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react';
+
+import useRooms from '../../../hooks/useRooms';
+import Rooms from '../Rooms';
+import Room from '../../../models/rooms';
+
+jest.mock('../../../hooks/useRooms');
+
+const mockUseRooms = useRooms as jest.MockedFunction<typeof useRooms>;
+
+beforeEach(() => {
+  mockUseRooms.mockReset();
+});
+
+describe('Rooms component', () => {
+  it('should show the error screen if an error occurs', () => {
+    mockUseRooms.mockReturnValueOnce({
+      rooms: [],
+      loading: false,
+      error: new Error('Server error'),
+    });
+
+    render(<Rooms />);
+
+    const errorScreen = screen.getByTestId('rooms-error');
+
+    expect(errorScreen).toBeInTheDocument();
+  });
+
+  it('should show the loading screen if it is loading', () => {
+    mockUseRooms.mockReturnValueOnce({
+      rooms: [],
+      loading: true,
+      error: null,
+    });
+
+    render(<Rooms />);
+
+    const loadingScreen = screen.getByTestId('rooms-loading');
+
+    expect(loadingScreen).toBeInTheDocument();
+  });
+
+  it('should show the rooms list if they are fetched', () => {
+    const rooms: Room[] = [
+      { name: 'room1', thumbnail: 'thumbnail', spots: 10 },
+      { name: 'room2', thumbnail: 'thumbnail', spots: 10 },
+      { name: 'room3', thumbnail: 'thumbnail', spots: 10 },
+    ];
+
+    mockUseRooms.mockReturnValueOnce({
+      rooms,
+      loading: false,
+      error: null,
+    });
+
+    render(<Rooms />);
+
+    const roomsScreen = screen.getByTestId('rooms-list');
+
+    expect(roomsScreen).toBeInTheDocument();
+  });
+});
